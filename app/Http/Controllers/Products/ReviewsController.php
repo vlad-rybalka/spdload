4ddf90fe410f8489f8b0325bb13177737df4c5e7
Vlad Rybalka <vlad.rybalka.ua@gmail.com>
2020-11-11 15:58:21 +0200

    add Products\ReviewsController

---
+++ app/Http/Controllers/Products/ReviewsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Products;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReviewsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+}
