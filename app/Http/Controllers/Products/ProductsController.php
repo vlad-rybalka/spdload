8afcafcb7b67006376b686d4d5b5fbd0b04b189c
Vlad Rybalka <vlad.rybalka.ua@gmail.com>
2020-11-11 15:54:15 +0200

    add ProductsController

---
+++ app/Http/Controllers/Products/ProductsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Products;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function store()
+    {
+
+    }
+
+    public function destroy($id)
+    {
+
+    }
+}
