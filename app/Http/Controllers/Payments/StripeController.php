218c5b0c735aeef13343fa4c908e38b43e067692
Vlad Rybalka <vlad.rybalka.ua@gmail.com>
2020-11-11 16:18:38 +0200

    add Payments\StripeController

---
+++ app/Http/Controllers/Payments/StripeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Payments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StripeController extends Controller
+{
+    public function store()
+    {
+
+    }
+}
